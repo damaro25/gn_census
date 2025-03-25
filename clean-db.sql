286e12d41295c0baa93c752615770e7664906be5
Laby Damaro CAMARA <labydamarocamara87@gmail.com>
2025-03-25 19:29:38 +0000

    clean-db.sql

---
+++ clean-db.sql
@@ -0,0 +1,5 @@
+delete from census_presentiel_files;
+delete from census_presentiel_enumerator;
+delete from census_presentiel;
+delete from census_teams;
+update census_applications set affecter = null;
